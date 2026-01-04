58d3328c815786dff20617e440903e24c7036f3d
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2026-01-04 10:30:52 -0700

    setup initial styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 import styled from "styled-components";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: lightgray;
+const StyledApp = styled.div`
+  padding: 20px;
 `;
 
 function App() {
   return (
     <div>
-      <H1>Hello World</H1>
+      <>
+        <GlobalStyles />
+        <StyledApp>
+          <Row>
+            <Row type="horizontal">
+              <Heading as="h1">The Wild Oasis</Heading>
+              <div>
+                <Heading as="h2">Admin Page</Heading>
+                <Button size="medium" variation="primary">
+                  Check In
+                </Button>
+                <Button size="medium" variation="secondary">
+                  Check Out
+                </Button>
+              </div>
+            </Row>
+            <Row>
+              <Heading as="h3">Form</Heading>
+              <div>
+                <Input placeholder="Type here..."></Input>
+                <Input placeholder="Type here..." />
+              </div>
+            </Row>
+          </Row>
+        </StyledApp>
+      </>
     </div>
   );
 }
